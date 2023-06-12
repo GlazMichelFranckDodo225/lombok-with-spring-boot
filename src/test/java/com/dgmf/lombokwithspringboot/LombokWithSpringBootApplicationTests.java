b8c483f650633ac89b941e88464df84c6d44c290
FranckDodo91 <devlogappdgmf@gmail.com>
2023-06-12 10:04:15 +0200

    Project Initialization

---
+++ src/test/java/com/dgmf/lombokwithspringboot/LombokWithSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dgmf.lombokwithspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LombokWithSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
