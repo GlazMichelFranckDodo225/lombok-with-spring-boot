ef02f6f4f77160425b433e030ad65afbcc63fe42
FranckDodo91 <devlogappdgmf@gmail.com>
2023-06-17 07:55:21 +0200

    Display of Attributes of the "Student" entity in the console ==> Ok

--- src/main/java/com/dgmf/lombokwithspringboot/Application.java
+++ src/main/java/com/dgmf/lombokwithspringboot/Application.java
@@ -1,5 +1,6 @@
 package com.dgmf.lombokwithspringboot;
 
+import com.dgmf.lombokwithspringboot.entity.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,15 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+
+		Student student = new Student();
+		student.setId(100L);
+		student.setFirstName("John");
+		student.setLastName("Doe");
+
+		System.out.println(student.getId());
+		System.out.println(student.getFirstName());
+		System.out.println(student.getLastName());
 	}
 
 }
